c64a895c2c6aa8eab2f222558db9d3c31233c9fc
AndrewSkelly <andrewskelly@hotmail.co.uk>
2024-09-16 11:40:09 +0100

    Finalized for first build

--- src/interfaces/Client.ts
+++ src/interfaces/Client.ts
@@ -1,20 +1,19 @@
 // For viewing and deleting clients
-export interface ClientView {
-    clientid: number;       // `clientid` is required
-    name: string | null;    // Must be string or null nothing else
-    email: string | null;   // Must be string or null nothing else
-    phone: string | null;   // Must be string or null nothing else
-    tags: string | null;    // Must be string or null nothing else
-    notes: string | null;   // Must be string or null nothing else
-  }
-  
-  // For adding new clients
-  export interface ClientAdd {
-    clientid?: number;      // `clientid` is optional and can be null, DB will handle value
-    name: string | null;    // Must be string or null nothing else
-    email: string | null;   // Must be string or null nothing else
-    phone: string | null;   // Must be string or null nothing else
-    tags: string | null;    // Must be string or null nothing else
-    notes: string | null;   // Must be string or null nothing else
-  }
-  
+export interface ClientDetails {
+  clientid: number;       // `clientid` is required
+  name: string | null;    // Must be string or null nothing else
+  email: string | null;   // Must be string or null nothing else
+  phone: string | null;   // Must be string or null nothing else
+  tags: string | null;    // Must be string or null nothing else
+  notes: string | null;   // Must be string or null nothing else
+}
+
+// For adding new clients
+export interface AddClientDetails {
+  clientid?: number;      // `clientid` is optional and can be null, DB will handle value
+  name: string | null;    // Must be string or null nothing else
+  email: string | null;   // Must be string or null nothing else
+  phone: string | null;   // Must be string or null nothing else
+  tags: string | null;    // Must be string or null nothing else
+  notes: string | null;   // Must be string or null nothing else
+}
