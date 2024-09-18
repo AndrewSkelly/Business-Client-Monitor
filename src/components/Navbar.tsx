bbbff4ceb7da4dce47a8de4bde058012680de5bd
AndrewSkelly <andrewskelly@hotmail.co.uk>
2024-09-18 02:36:20 +0100

    Began Mobile Friendly Design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.scss'
-import Logo from '../assets/icons/ClientLogLogo.png'
+import './Navbar.scss';
+import Logo from '../assets/icons/ClientLogLogo.png';
 
 const Navbar: React.FC = () => {
-    return (
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-logo"><img src={Logo} alt="" /></li>
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/client-log" className="nav-link">Client List</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/service-history" className="nav-link">Service History</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/analytics" className="nav-link">Analytics</Link>
-          </li>
-        </ul>
-      </nav>
-    );
+  // State to control whether the navbar is open or closed
+  const [isOpen, setIsOpen] = useState(true); // Default to false, meaning the navbar is closed initially
+
+  // Function to toggle navbar visibility
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
   };
 
-export default Navbar
+  return (
+    <>
+      {/* Hamburger icon, only shown when the navbar is closed */}
+      {!isOpen && (
+        <div className="hamburger" onClick={toggleNavbar}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      )}
+
+      {/* Navbar that appears when isOpen is true */}
+      {isOpen && (
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li className="nav-logo">
+              <img src={Logo} alt="Logo" />
+            </li>
+            <li className="nav-close" onClick={toggleNavbar}>
+              X
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/client-log" className="nav-link">Client List</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/service-history" className="nav-link">Service History</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/analytics" className="nav-link">Analytics</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
